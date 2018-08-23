37a26c44e477f0dac254155629e28e5f33e0ab88
d-smith <doug.smith.mail@gmail.com>
2018-08-23 10:24:04 -0700

    Test update

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # sls-middy
 
-Serverless example using [middy]() for middleware.
+## Overview
+
+Serverless example using [middy](https://github.com/middyjs/middy) for middleware.
 
 This shows an example of how to wrap a simple handler with some middleware to take care of parsing the payload, validating it against a schema, etc.
 
@@ -16,3 +18,7 @@ Post your payload thusly:
 ```console
  curl -H 'x-api-key:kkkkk' https://<apiid>.execute-api.<region>.amazonaws.com/dev/signup -d '{"first":"Jim","last":"Pansey","email":"jp@example.com"}' -H 'Content-Type:application/json'
  ```
+
+ ## Tests
+
+ * Integration tests - for these, we invoke our lambda handlers directy with stubbed inputs (event, content, callback), and harvest the results in the callback to assert expected behavior.
--- tests/test_cases/signup-user.js
+++ tests/test_cases/signup-user.js
@@ -1,8 +1,8 @@
 const expect = require('chai').expect;
 const when = require('../steps/when');
 
-describe(`When we invoke the info endpoint`, async function() {
-    it(`Should return the sign up now page`, async function() {
+describe(`When we invoke the info endpoint`, async () => {
+    it(`Should return the sign up now page`, async () => {
         let res = await when.we_invoke_get_signup_info();
         expect(res.statusCode).to.equal(200);
         expect(res.body.message).to.equal('Sign up plz');
@@ -10,8 +10,8 @@ describe(`When we invoke the info endpoint`, async function() {
 });
 
 
-describe(`When we invoke the sign up endpoint with a valid payload`, async function() {
-    it(`Should indicate signup succeeded`, async function(){
+describe(`When we invoke the sign up endpoint with a valid payload`, async () => {
+    it(`Should indicate signup succeeded`, async () => {
         let payload = {
             first: 'firstName',
             last: 'lastName',
@@ -23,5 +23,14 @@ describe(`When we invoke the sign up endpoint with a valid payload`, async funct
         console.log(res.body);
         expect(res.body.message).to.equal('Sign up succeeded');
         
+    });
+});
+
+describe(`When we signup with an invalid payload`, async () => {
+    it(`Should return an error indicating failed validation`, async () => {
+        let payload = {};
+        let res = await when.we_invoke_signup(payload);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body).to.include('failed validation');
     });
 });

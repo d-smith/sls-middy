161f907e6ed4e6bd4c609b761e860eb18378ca01
d-smith <doug.smith.mail@gmail.com>
2018-08-24 08:35:39 -0700

    Refactored when.js

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,8 +1,7 @@
 const APP_ROOT = '../../';
 const _ = require('lodash');
 
-const we_invoke_signup = (payload) => {
-    let handler = require(`${APP_ROOT}/functions/handler`).signup;
+const viaHandler = (event, handler) => {
     return new Promise((resolve, reject) => {
         
         let callback = function (err, response) {
@@ -19,32 +18,22 @@ const we_invoke_signup = (payload) => {
         };
 
         let context = {};
-        let event = {};
-        event.body = payload;
         handler(event, context, callback);
     });
+};
+
+const we_invoke_signup = (payload) => {
+    let handler = require(`${APP_ROOT}/functions/handler`).signup;
+    let event = {
+        body: payload
+    };
+
+    return viaHandler(event, handler);
 }
 
 const we_invoke_get_signup_info = () => {
     let handler = require(`${APP_ROOT}/functions/handler`).info;
-
-    return new Promise((resolve, reject) => {
-        let context = {};
-        let callback = function (err, response) {
-            if (err) {
-                reject(err);
-            } else {
-                let contentType = _.get(response, 'headers.Content-Type', 'unknown');
-                if (response.body && contentType === 'application/json') {
-                    response.body = JSON.parse(response.body);
-                }
-
-                resolve(response);
-            }
-        };
-
-        handler({}, context, callback);
-    });
+    return viaHandler({}, handler);
 }
   
 

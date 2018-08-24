249c90455b9a92fc10b2da3bd3ed0e44a61cb431
d-smith <doug.smith.mail@gmail.com>
2018-08-24 10:19:54 -0700

    Added acceptance tests that go against the main endpoint

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,5 +1,74 @@
 const APP_ROOT = '../../';
 const _ = require('lodash');
+const rq = require('request');
+const mode    = process.env.TEST_MODE;
+
+const postOptions = (opts) => {
+    let headers = {};
+    headers['Content-Type'] = 'application/json';
+
+    apiKey = _.get(opts, 'apiKey');
+    if(apiKey) {
+        headers['x-api-key'] = apiKey;
+    }
+
+    return {
+        method: 'POST',
+        body: _.get(opts, 'body'),
+        json: true,
+        headers: headers
+    };
+}
+
+const getOptions = (opts) => {
+    let headers = {};
+    apiKey = _.get(opts, 'apiKey');
+    if(apiKey) {
+        headers['x-api-key'] = apiKey;
+    }
+
+    return {
+        method: 'GET',
+        json: true,
+        headers: headers
+    };
+}
+
+const viaHttp = (relPath, method, opts) => {
+    let root = process.env.TEST_ROOT;
+    let url = `${root}/${relPath}`;
+
+    var options;
+
+    switch(method) {
+        case 'GET':
+            options = getOptions(opts);
+            break;
+        case 'POST':
+            options = postOptions(opts);
+            break;
+        default:
+            throw new Error('Method must be GET or POST');
+            break;
+    }
+
+    options.uri = url;
+    console.log(`Invoking rq with options ${JSON.stringify(options)}`);
+
+    return new Promise((resolve, reject) => {
+        let callback = function(err, response, body) {
+            if(err) {
+                reject(err);
+            } else {
+//                console.log(`response is ${JSON.stringify(response)}`);
+//                console.log(`body is ${JSON.stringify(body)}`);
+                resolve(response);
+            }
+        }
+
+        rq(options, callback);
+    });
+}
 
 const viaHandler = (event, handler) => {
     return new Promise((resolve, reject) => {
@@ -23,17 +92,36 @@ const viaHandler = (event, handler) => {
 };
 
 const we_invoke_signup = (payload) => {
-    let handler = require(`${APP_ROOT}/functions/handler`).signup;
-    let event = {
-        body: payload
-    };
+    if(mode === 'http') {
+        let opts = {
+            body: payload,
+            apiKey: process.env.TEST_KEY
+        }
 
-    return viaHandler(event, handler);
+        return viaHttp('signup', 'POST', opts);
+    } else {
+        let handler = require(`${APP_ROOT}/functions/handler`).signup;
+        let event = {
+            body: payload
+        };
+
+        return viaHandler(event, handler);
+    }
+    
 }
 
 const we_invoke_get_signup_info = () => {
-    let handler = require(`${APP_ROOT}/functions/handler`).info;
-    return viaHandler({}, handler);
+    if(mode === 'http') {
+        let opts = {
+            apiKey: process.env.TEST_KEY
+        }
+
+        return viaHttp('info', 'GET', opts);
+    } else {
+        let handler = require(`${APP_ROOT}/functions/handler`).info;
+        return viaHandler({}, handler);
+    }
+    
 }
   
 
